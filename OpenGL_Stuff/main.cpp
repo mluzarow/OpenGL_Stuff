b8b7db0378c4fbc08350bec3a665c27cedabe644
mluzarow <mluzarow@purdue.edu>
2015-01-02 23:19:12 -0600

    Added functions for drawing some primitives

--- OpenGL_Stuff/main.cpp
+++ OpenGL_Stuff/main.cpp
@@ -1,9 +1,139 @@
-//#include "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include/GL/glew.h"
-//#include "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include/GL/freeglut.h"
 #include "glew.h"
 #include "freeglut.h"
 
+#include "main.h"
 
+//Variables
+bool* KeyStates = new bool[256]; //Array of booleans for current key state
+// false = Not pressed
+// true = Pressed
+bool* SpecialKeyStates = new bool[256]; //Array of booleans for current special key state
+// false = Not pressed
+// true = Pressed
+
+void Special_Key_Operations (void) {
+	if (SpecialKeyStates[GLUT_KEY_LEFT]) {
+		//stuff
+	}
+}
+
+//Detect special key presses
+void Special_Key_Pressed (int key, int x, int y) {
+	SpecialKeyStates[key] = true; //Current special key is pressed
+}
+
+//Detect special key releases
+void Special_Key_Released (int key, int x, int y) {
+	SpecialKeyStates[key] = false; //Current special key is released
+}
+
+//Perform key operations
+void Key_Operations (void) {
+	if (KeyStates['b']) {
+		drawPoint(200, 200);
+	} else if (KeyStates['a']) {
+		drawLine(250, 250, 450, 450);
+	}
+
+	glutPostRedisplay();
+}
+
+//Detect key presses
+void Key_Pressed (unsigned char key, int x, int y) {
+	KeyStates[key] = true; //Current key is pressed
+}
+
+//Detect key releases
+void Key_Released (unsigned char key, int x, int y) {
+	KeyStates[key] = false; //Current key is not pressed
+}
+
+//Draws a line of pixels from (250, 250) to (500, 500)
+void drawLine(int x1, int y1, int x2, int y2) {
+	
+	
+	glBegin(GL_LINES);
+	glVertex2d(x1, y1);
+	glVertex2d(x2, y2);
+	glEnd();
+
+	
+}
+
+//Draws a pixel at (250, 250)
+void drawPoint(int x, int y) {
+
+	glBegin(GL_POINTS);
+	glVertex2f(x,y);
+	glEnd();
+
+}
+
+//Store drawing code
+void Display (void) {
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f(3.0,0.4,0.2); 
+	glPointSize(1.0); 
+
+	Key_Operations(); //Get key processing
+
+	glFlush();
+/*
+	int f, x = 0;
+	int f_n[501];
+	int index = 0;
+	
+	for (x = 0; x < 501; x++) {
+		f_n[x] = x;
+	}
+
+	glClear(GL_COLOR_BUFFER_BIT);  
+    glColor3f(3.0,0.4,0.2); 
+    glPointSize(1.0); 
+
+	for (int i = 0; i < 501; i++) {
+		lineDraw(f_n[i], 100);
+	}
+	
+	glBegin(GL_LINES);
+    glVertex2d(, 65);
+	glVertex2d(345, 432);
+    glEnd();
+
+	glFlush();
+	*/
+}
+
+void init(void) {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //Clear background of window
+ 
+	glViewport(0, 0, 500, 500);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 500, 0, 500, 1, -1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+}
+
+int main (int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("Scribbles");
+
+	init(); //Initialize window
+	glutDisplayFunc(Display); //Tell GLUT to use Display method
+
+	glutKeyboardFunc(Key_Pressed); //Tell GLUT to use Key_Pressed method
+	glutKeyboardUpFunc(Key_Released); //Tell GLUT to use Key_Released method
+	glutSpecialFunc(Special_Key_Pressed); //Tell GLUT to use Special_Key_Pressed method
+	glutSpecialUpFunc(Special_Key_Released); //Tell GLUT to use Special_Key_Released method
+
+	glutMainLoop();
+}
+/*
 //Variables
 bool* KeyStates = new bool[256]; //Array of booleans for current key state
 // false = Not pressed
@@ -164,4 +294,6 @@ int main (int argc, char **argv) {
 
 	glutMainLoop(); //Throw into graphics loop
 }
+
+*/
 
---
+++ OpenGL_Stuff/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void drawPoint(int, int);
+void drawLine(int, int, int, int);
+
+void Key_Operations (void);
+void Key_Pressed (unsigned char, int, int);
+void Key_Released (unsigned char, int, int);
+
+void Display (void);
+void init(void);
+
+#endif
